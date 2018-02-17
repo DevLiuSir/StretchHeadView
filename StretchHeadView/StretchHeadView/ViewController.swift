00daf742588f8bb0a959c561a31e418a7a864acd
ChinaHackers <93428739@qq.com>
2018-02-17 14:58:09 +0800

    support swift4.0.3

--- StretchHeadView/StretchHeadView/ViewController.swift
+++ StretchHeadView/StretchHeadView/ViewController.swift
@@ -21,7 +21,6 @@ private let navigationH: CGFloat = 44
 /// 状态栏的高度
 private let statusH: CGFloat = 20
 
-
 /// 深绿色
 private let darkGreen = UIColor(hue:0.40, saturation:0.78, brightness:0.68, alpha:1.00)
 
@@ -51,7 +50,6 @@ class ViewController: UIViewController {
         configHeaderView()
     }
     
-    
     // MARK: - 视图即将出现时, 调用
     override func viewWillAppear(_ animated: Bool) {
         
@@ -69,28 +67,32 @@ class ViewController: UIViewController {
         return .lightContent
     }
     
+    
     /// 配置TableView
     private func configTableView() {
+        
+        // 判断下系统版本
+        if #available(iOS 11.0, *) {
+            tableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentBehavior.never
+        } else {
+            // Fallback on earlier versions
+        }
         
         // 设置表格顶部间距, 使得HaderView不被遮挡
         tableView.contentInset = UIEdgeInsets(top: headerviewH, left: 0, bottom: 0, right: 0)
         
         // 设置指示器的间距, 等于表格顶部的间距
         tableView.scrollIndicatorInsets = tableView.contentInset
-        
         tableView.dataSource = self
         tableView.delegate = self
         
         // 注册cell
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: CellID)
-        
-        
     }
     /// 配置HaderView
     private func configHeaderView() {
         
         Headerview = UIView(frame: CGRect(x: 0.0, y: 0.0, width: screenW, height: headerviewH))
-        
         Headerview.backgroundColor = darkGreen
         
         //HeaderImage = UIImageView(frame: CGRect(x: 0, y: 0, width: screenW, height: headerviewH))
@@ -115,14 +117,12 @@ class ViewController: UIViewController {
         HeaderImage.clipsToBounds = true
         
         
-        //MARK: 添加分割线: 1个像素点
-        
+        // 添加分割线: 1个像素点
         // 1个像素点 / 分辨率
         let lineH = 1 / UIScreen.main.scale
         
         lineView = UIView(frame: CGRect(x: 0, y: headerviewH - lineH, width: screenW, height: lineH))
         lineView.backgroundColor = UIColor.lightGray
-        
         
         // 添加控件
         Headerview.addSubview(lineView)
@@ -138,9 +138,7 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 100
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: CellID, for: indexPath)
         cell.textLabel?.text = "\(indexPath.row)"
         return cell
@@ -153,50 +151,15 @@ extension ViewController: UITableViewDataSource {
 extension ViewController: UITableViewDelegate {
  
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        
-        /*
-         
+/*
          contentOffset: 即偏移量,contentOffset.y = 内容的顶部和frame顶部的差值,contentOffset.x = 内容的左边和frame左边的差值.
          contentInset:  即内边距,contentInset = 在内容周围增加的间距(粘着内容),contentInset的单位是UIEdgeInsets
-         
-         */
+*/
         // 偏移量: -200 + 顶部边距: 200, 等于0
         let offsetY = scrollView.contentOffset.y + scrollView.contentInset.top
-        print(offsetY)
-
-/*
-        // 放大图像
-        if offsetY <= 0 {
-      
-            // 调整HeaderView\ HeaderImage
-            Headerview.frame.origin.y = 0
-            
-            // 增大 Headerview 高度
-            Headerview.frame.size.height = headerviewH - offsetY
-            Headerview.alpha = 1
-         
-            
-        } else {    // 整体移动
-            Headerview.frame.size.height = headerviewH
-            Headerview.frame.origin.y = -offsetY
-            
-            /// HeaderView最小的Y值
-            let headerViewMinY = headerviewH - navigationH - statusH
-
-            // min函数: 取最小值
-            Headerview.frame.origin.y = -min(headerViewMinY, offsetY)
-            
-            
-            // 设置透明度
-            // 根据输出, 得知当 offsetY / headerViewMinY == 1时,不可见图像
-            //print(offsetY / headerViewMinY)
-            let progress = 1 - (offsetY / headerViewMinY)
-            
-            HeaderImage.alpha = progress
-    
-        }
-*/
+        //print(offsetY)
         
+        // 放大图像
         guard offsetY <= 0 else {
             
             // MARK: 整体移动
@@ -226,14 +189,10 @@ extension ViewController: UITableViewDelegate {
         Headerview.frame.size.height = headerviewH - offsetY
         Headerview.alpha = 1
 
-        
         // 设置分割线的位置
         lineView.frame.origin.y = Headerview.frame.size.height - lineView.frame.size.height
         
         // 图像视图高度 = headView高度
         HeaderImage.frame.size.height = Headerview.frame.size.height
-        
     }
-    
 }
-
